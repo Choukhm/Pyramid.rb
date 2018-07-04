7f343e059e1dce09ac7e972a09c76e380d3feeee
Choukhm <archoukhmane@gmail.com>
2018-07-04 13:50:01 +0200

    Update pyramid.rb

--- pyramid.rb
+++ pyramid.rb
@@ -3,14 +3,14 @@ etages = gets.chomp
 blanc = etages.to_i
 if etages.to_i > 25
     puts "Il y'a trop d'étages"
-    
-    elsif etages.to_i < 0
-    puts "Il y'a pas assez d'étages"
-    else
+elsif 
+        etages.to_i < 0
+        puts "Il y'a pas assez d'étages"
+else 
     i=1
     blanc.times do
-        puts " "*blanc + "#"*i
-        blanc-=1
-        i+=1
+    puts " "*blanc + "#"*i
+    blanc-=1
+    i+=1
     end
 end
